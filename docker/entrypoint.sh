2ec45875d30500a49faa16f3ed9b5c606c1ab096
rga5321 <n6g9pgl42@mozmail.com>
2025-07-29 19:10:34 +0200

    trying to fix Dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,19 +5,6 @@
 # source /home/appuser/env.local
 # set +a
 
-# Sobrescribir .env con env.local
-cp /home/appuser/env.local /home/appuser/todoist2ebook/.env
-cp /home/appuser/env.local /home/appuser/vars.py
-
-# Generar vars.py a partir de env.local
-# Suponiendo que env.local tiene VAR1=valor1
-# el script transformará eso a:
-# VAR1 = "valor1"
-
-# > /home/appuser/todoist2ebook/vars.py
-# while IFS='=' read -r key value; do
-#     echo "$key = \"$value\"" >> /home/appuser/todoist2ebook/vars.py
-# done < /home/appuser/env.local
 
 # Finalmente ejecutar main.py
 cd /home/appuser/todoist2ebook
